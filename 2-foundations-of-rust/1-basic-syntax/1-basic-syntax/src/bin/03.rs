aa1e45dfa209fd76bb17251cbe9e2ca86e150004
spx <spx@>
2024-10-07 18:33:08 +0300

    lab1
    Signed-off-by: spx <spx@>

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -2,6 +2,8 @@ fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
     // TODO
+    let max = input.iter().max().unwrap();
+    let min = input.iter().min().unwrap();
 
-    println!("{} is largest and {} is smallest");
+    println!("{max} is largest and {min} is smallest");
 }
